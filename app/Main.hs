a62666f3f9f1257ec8079f2b48b02f376d951eb1
Kazu Yamamoto <kazu@iij.ad.jp>
2017-12-27 13:09:32 +0900

    refactoring.

--- app/Main.hs
+++ app/Main.hs
@@ -10,34 +10,40 @@ import qualified Data.ByteString.Lazy as BL
 
 main :: IO ()
 main = do
-    let host = "localhost"
+    let localhost = "localhost"
         listenPort = "9876"
         connectPort = "6789"
-    fromClient <- myserve host listenPort
-    toServer <- myconnect host connectPort
-    void $ forkIO $ proxy toServer fromClient "s->c"
-    proxy fromClient toServer "c->s"
+    proxy localhost listenPort connectPort
 
-proxy :: Socket -> Socket -> String -> IO ()
-proxy s1 s2 str = forever $ do
+proxy :: HostName -> ServiceName -> ServiceName -> IO ()
+proxy localhost listenPort connectPort = do
+    fromClient <- serverSocket localhost listenPort
+    toServer <- clientSocket localhost connectPort
+    void $ forkIO $ relay toServer fromClient "s->c"
+    relay fromClient toServer "c->s"
+
+relay :: Socket -> Socket -> String -> IO ()
+relay s1 s2 str = forever $ do
     payload <- recv s1 4096
     putStrLn str
     sendAll s2 $ BL.fromStrict payload
 
-myhints :: AddrInfo
-myhints = defaultHints { addrSocketType = Stream }
+serverSocket :: HostName -> ServiceName -> IO Socket
+serverSocket host port = do
+    let hints = defaultHints {
+                addrFlags = [AI_PASSIVE]
+              , addrSocketType = Stream
+              }
+    addr:_ <- getAddrInfo (Just hints) (Just host) (Just port)
+    sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
+    bind sock (addrAddress addr)
+    listen sock 1
+    fst <$> accept sock
 
-myserve :: HostName -> ServiceName -> IO Socket
-myserve host port = do
-  (addr:_) <- getAddrInfo (Just myhints) (Just host) (Just port)
-  sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
-  bind sock (addrAddress addr)
-  listen sock 1
-  fst <$> accept sock
-
-myconnect :: HostName -> ServiceName -> IO Socket
-myconnect host port = do
-  (addr:_) <- getAddrInfo (Just myhints) (Just host) (Just port)
-  sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
-  connect sock (addrAddress addr)
-  return sock
+clientSocket :: HostName -> ServiceName -> IO Socket
+clientSocket host port = do
+    let hints = defaultHints { addrSocketType = Stream }
+    addr:_ <- getAddrInfo (Just hints) (Just host) (Just port)
+    sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
+    connect sock (addrAddress addr)
+    return sock
