412ad7a20e02df17bcf4580f13fed430efcc6031
Kazu Yamamoto <kazu@iij.ad.jp>
2017-12-27 13:25:57 +0900

    implementing client.

--- app/Main.hs
+++ app/Main.hs
@@ -6,35 +6,44 @@ import Network.Socket hiding (recv)
 import Network.Socket.ByteString (recv)
 import Network.Socket.ByteString.Lazy (sendAll)
 
+import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BL
 
 main :: IO ()
 main = do
-    let localhost = "localhost"
+    let host = "localhost"
         listenPort = "9876"
         connectPort = "6789"
-    proxy localhost listenPort connectPort
+    _ <- forkIO $ proxy host listenPort connectPort
+    client host listenPort
+
+client :: HostName -> ServiceName -> IO ()
+client host port = do
+    s <- clientSocket host port
+    forever $ do
+        sendAll s $ BL.fromStrict $ BS.pack $ replicate 5000 170
+        recv s 4096
 
 proxy :: HostName -> ServiceName -> ServiceName -> IO ()
-proxy localhost listenPort connectPort = do
-    fromClient <- serverSocket localhost listenPort
-    toServer <- clientSocket localhost connectPort
+proxy host listenPort connectPort = do
+    fromClient <- serverSocket listenPort
+    toServer <- clientSocket host connectPort
     void $ forkIO $ relay toServer fromClient "s->c"
     relay fromClient toServer "c->s"
 
 relay :: Socket -> Socket -> String -> IO ()
 relay s1 s2 str = forever $ do
-    payload <- recv s1 4096
+    msg <- recv s1 4096
     putStrLn str
-    sendAll s2 $ BL.fromStrict payload
+    sendAll s2 $ BL.fromStrict msg
 
-serverSocket :: HostName -> ServiceName -> IO Socket
-serverSocket host port = do
+serverSocket :: ServiceName -> IO Socket
+serverSocket port = do
     let hints = defaultHints {
                 addrFlags = [AI_PASSIVE]
               , addrSocketType = Stream
               }
-    addr:_ <- getAddrInfo (Just hints) (Just host) (Just port)
+    addr:_ <- getAddrInfo (Just hints) (Just "localhost") (Just port)
     sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
     bind sock (addrAddress addr)
     listen sock 1
